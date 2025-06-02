9ab7c4323bd07d0e597d8a6dea4d49a971dac9a9
zilla998 <coldssielenssams@gmail.com>
2025-06-02 19:49:51 +0300

    + commit

--- products/context_processors/cart.py
+++ products/context_processors/cart.py
@@ -1,7 +1,11 @@
+from products.models import Cart
+
+
 def count_cart(request):
 
     cart = request.session.get('cart', {})
-
+    if request.user.is_authenticated:
+        cart = Cart.objects.get(user=request.user).products.all()
     return {
         'cart_count': len(cart)
     }
